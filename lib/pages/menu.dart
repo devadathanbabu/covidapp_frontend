47d79140e5fc3f0a381ee7683592b2f5035f6ebd
devadathanbabu <devadathanbabu27@gmail.com>
2024-02-08 20:11:56 +0530

    menu completed

---
+++ lib/pages/menu.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+
+  @override
+  State<MenuPage> createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("COVIDAPP",style: TextStyle(fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.indigo.withOpacity(0.9),
+      ),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(width: 20,),
+          Container(
+             height: 390,
+             width: 390,
+           decoration: BoxDecoration(
+             borderRadius: BorderRadius.circular(15),
+             color: Colors.indigo.withOpacity(0.6),
+           ),
+
+            margin: EdgeInsets.all(30),
+            padding: EdgeInsets.all(60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                     SizedBox(height: 10,),
+                    Icon(Icons.keyboard_alt_outlined,size: 40,),
+                    TextButton(
+                        onPressed: (){}, child: Text("Patient Entry",
+                      style: TextStyle(fontSize: 20,
+                      color: Colors.black87),
+                    ))
+                  ],
+                ),
+                 SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 17,),
+                    Icon(CupertinoIcons.eye_fill,size: 40,),
+                    TextButton(onPressed: (){},
+                        child: Text("Patient Details",
+                        style: TextStyle(fontSize: 20,
+                        color: Colors.black87),))
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
